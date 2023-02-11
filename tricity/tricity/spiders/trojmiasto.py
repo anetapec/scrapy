baa139ca625acbb120f25ee98978989dc93c9413
Aneta Gawron <aneta.pecka@gmail.com>
2023-02-11 22:21:19 +0100

    Poprawilam selektory , ale niestety dalej pobiera jeden dom . tyle ze juz nie w nieskonczonosc....

--- tricity/tricity/spiders/trojmiasto.py
+++ tricity/tricity/spiders/trojmiasto.py
@@ -7,14 +7,14 @@ class HousesspiderSpider(scrapy.Spider):
     start_urls = ['https://ogloszenia.trojmiasto.pl/nieruchomosci/dom/gdynia/']
 
     def parse(self, response):
-        houses = response.xpath("//div[@id = 'ogl__list__wrap']").get()
+        houses = response.xpath('//div[contains(@id,"ogl-")]').getall()
 
         for house in houses:
             house_item = HouseItem()
-            house_item['price'] = response.xpath("//p[@class = 'list__item__price__value']//text()").get()
-            house_item['price_per_meter'] = response.css('p.list__item__details__info.details--info--price::text').get()
+            house_item['price'] = response.css('p.list__item__price__value::text').get().replace('zł', '')
+            house_item['price_per_meter'] = response.css('p.list__item__details__info.details--info--price::text').get().replace('zł/m', '')
             house_item['url'] = response.css('a.list__item__content__title__name.link').attrib['href']
-            house_item['area'] = response.xpath("//p[@class='list__item__details__icons__element__desc']//text()").extract_first()
-            house_item['number_of_rooms'] = response.xpath('//ul/li[2]/div/p[2]//text()').extract_first()
+            house_item['area'] = response.xpath("//p[@class='list__item__details__icons__element__desc']//text()").get().replace('\n                                            ', '').replace('\n                                    ', '')
+            house_item['number_of_rooms'] = response.xpath('//ul/li[2]/div/p[2]//text()').get().replace('\n                                            ', '').replace('\n                                    ', '')
 
             yield house_item
