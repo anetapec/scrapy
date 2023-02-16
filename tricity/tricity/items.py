46f19ea5e1eb58476827fc0f11309f7073c89060
Aneta Gawron <aneta.pecka@gmail.com>
2023-02-17 00:05:40 +0100

    Poki co cena, url i powierzchnia

--- tricity/tricity/items.py
+++ tricity/tricity/items.py
@@ -3,8 +3,8 @@ from scrapy import Item, Field
 
 
 class HouseItem(Item):
-    price = Field() 
-    price_per_meter = Field()
-    url = Field() 
-    area = Field() 
-    number_of_rooms = Field()
+    price = Field()
+    #price_per_meter = Field()
+    url = Field()
+    area = Field()
+    #numbers_of_rooms = Field()
--- tricity/tricity/spiders/trojmiasto.py
+++ tricity/tricity/spiders/trojmiasto.py
@@ -1,27 +1,37 @@
 import scrapy
 from tricity.items import HouseItem
+import re
+
 
 class HousesspiderSpider(scrapy.Spider):
     name = 'tricity'
     allowed_domains = ['ogloszenia.trojmiasto.pl']
     start_urls = ['https://ogloszenia.trojmiasto.pl/nieruchomosci/dom/gdynia/']
 
+    
+    
     def parse(self, response):
-        houses = response.xpath('//div[contains(@id,"ogl-")]').getall()
-
+        houses = response.css('#ogl__list__wrap .list__item')
+        
         for house in houses:
             house_item = HouseItem()
-            house_item['price'] = response.css('p.list__item__price__value::text').get().replace('zł', '')
-            house_item['price_per_meter'] = response.css('p.list__item__details__info.details--info--price::text').get().replace('zł/m', '')
-            house_item['url'] = response.css('a.list__item__content__title__name.link').attrib['href']
-            house_item['area'] = response.xpath("//p[@class='list__item__details__icons__element__desc']//text()").get().replace('\n                                            ', '').replace('\n                                    ', '')
-            house_item['number_of_rooms'] = response.xpath('//ul/li[2]/div/p[2]//text()').get().replace('\n                                            ', '').replace('\n                                    ', '')
+
+            house_item['price'] = re.sub("[^0-9]", "", house.css('div.list__item__picture__price').get())
+            #house_item['price_per_meter'] = house.css('p.list__item__details__info.details--info--price::text').get()
+            house_item['url'] = house.css('a.list__item__content__title__name.link').attrib['href']
+            house_item['area'] = re.sub("[^0-9]", "",house.css('p.list__item__details__icons__element__desc::text').get())
+            #house_item['number_of_rooms'] = house.css('p.list__item__details__icons__element__desc::text')[1].get()
 
             yield house_item
+                
+            
 
-        next_page = response.css('span.pages__item__active').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+
+            
+        
+
+            
+            
 
             
 
