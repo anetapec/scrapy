3621ab20d6e147ea9423d3b107e4758b1cf1e55f
Aneta Gawron <aneta.pecka@gmail.com>
2023-02-21 12:21:39 +0100

    zeskrobalo ponad 6780 pozycji , nie uwzglednilo filtrow ktore narzucilismy na poczatku

--- tricity/tricity/spiders/trojmiasto.py
+++ tricity/tricity/spiders/trojmiasto.py
@@ -15,19 +15,23 @@ class HousesspiderSpider(scrapy.Spider):
         
         for house in houses:
             house_item = HouseItem()
-            #if HouseItem is None:
-            house_item['price'] = re.sub("[^0-9]", "", house.css('div.list__item__picture__price').get())
-            house_item['url'] = house.css('a.list__item__content__title__name.link').attrib['href']
-            house_item['numbers_of_rooms'] = re.sub("[^.0-9]", "", house.css('li.list__item__details__icons__element.details--icons--element--l_pokoi').get())
-            house_item['area'] = re.sub("[^.0-9]", "",house.css('p.list__item__details__icons__element__desc::text').get())
-            #house_item['price_per_meter'] = float(round((house_item['price']) / float(house_item['area'])), 2)
-            house_item['price_per_meter'] = float(house_item['price']) / float(house_item['area'])
             
-            
+            try:
                 
-            #continue
+                house_item['price'] = re.sub("[^0-9]", "", house.css('div.list__item__picture__price').get())
+                house_item['url'] = house.css('a.list__item__content__title__name.link').attrib['href']
+                house_item['numbers_of_rooms'] = re.sub("[^.0-9]", "", house.css('li.list__item__details__icons__element.details--icons--element--l_pokoi').get())
+                house_item['area'] = re.sub("[^.0-9]", "",house.css('p.list__item__details__icons__element__desc::text').get())
+                #house_item['price_per_meter'] = float(round((house_item['price']) / float(house_item['area'])), 2)
+                house_item['price_per_meter'] = float(house_item['price']) / float(house_item['area'])
+            
+            except: 
+                print("-")
 
             yield house_item
+
+            
+            
 
         
 
@@ -36,15 +40,4 @@ class HousesspiderSpider(scrapy.Spider):
         next_page = base_url + path_url
         if path_url is not None:
             yield response.follow(next_page, callback=self.parse)
-            
-
-
-            
-        
-
-            
-            
-
-            
-
             
