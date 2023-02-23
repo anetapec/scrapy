5c6cc263007424c5070c872a02534a95cb99c505
Aneta Gawron <aneta.pecka@gmail.com>
2023-02-23 23:45:47 +0100

    mongo piplines

--- tricity/tricity/settings.py
+++ tricity/tricity/settings.py
@@ -25,7 +25,8 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 25
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -62,10 +63,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "tricity.pipelines.TricityPipeline": 300,
-#}
-
+ITEM_PIPELINES = {
+    'reddit.pipelines.MongoPipeline': 300,
+}
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = 'sivji-sandbox'
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
