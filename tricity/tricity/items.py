a312dab0de7a09ff33c3a67d3307ba0eaac229cd
Aneta Gawron <aneta.pecka@gmail.com>
2023-03-06 21:57:16 +0100

    to co zrobilismy po poniedzialkowym zdzwonieniu

--- tricity/tricity/items.py
+++ tricity/tricity/items.py
@@ -7,4 +7,6 @@ class HouseItem(Item):
     url = Field()
     numbers_of_rooms = Field() 
     area = Field() 
-    
+    scrapping_date = Field()
+    #hash = Field()
+    #last_seen_date = Field()
--- tricity/tricity/pipelines.py
+++ tricity/tricity/pipelines.py
@@ -2,35 +2,26 @@ import pymongo
 #from .items import HouseItem
 from tricity import settings
 from datetime import datetime
-
+ 
 class MongoDBPipeline:
 
+    def set_scrapping_date(self):
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        return now
+
     def open_spider(self, spider):
-        connection = pymongo.MongoClient(settings.mongodb_uri)
-        db = connection[settings.mongodb_db]
+        self.scrapping_date = self.set_scrapping_date()
         self.client = pymongo.MongoClient(settings.mongodb_uri)
+        db = self.client[settings.mongodb_db]
         self.collection = db[settings.colection_name]
         #start with a clean database
         self.collection.delete_many({})
 
     def close_spider(self, spider):
         self.client.close()
-    
-    @property
-    def date_scraping(self):
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        return now
-    
-    def proces_item(self, item, spider):
-        item['date'] = self.date_scraping
+
+    def process_item(self, item, spider):
+        item['scrapping_date'] = self.scrapping_date
         data = dict(item)
         self.collection.insert_one(data)
-    
         return item
-    
-      
-        
-    
-   
-   
-    
