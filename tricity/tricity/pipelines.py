79ee486f48ec020f3d98639cde3e060888e815bd
Aneta Gawron <aneta.pecka@gmail.com>
2023-03-12 00:31:20 +0100

    jeszcze przed zaczeciem z dockerem

--- tricity/tricity/pipelines.py
+++ tricity/tricity/pipelines.py
@@ -27,7 +27,7 @@ class MongoDBPipeline:
         self.client.close()
 
     def process_item(self, item, spider):
-        item['price_per_meter'] = float(item['price']) / float(item['area'])
+        item['price_per_meter'] = str(round((float(item['price']) / float(item['area'])), 2))
         item['hash'] = self.set_hash(item)
         item['scrapping_date'] = self.scrapping_date
         data = dict(item)
