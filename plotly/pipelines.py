33ae0188d8d734eea1a8d752ab802995c9e670e9
anetapec <aneta.pecka@gmail.com>
2023-04-03 00:56:52 +0200

    zerknij na to

--- plotly/pipelines.py
+++ plotly/pipelines.py
@@ -9,7 +9,7 @@ def load_collection(self):
     df = pd.DataFrame(list(collection.find()))
     return df
 
-def count_avg_price(self):
+def count_avg_price_per_meter(self):
     avg_price = self.collection.aggregate([{
         '$group': {
             '_id': 'hash',
@@ -17,16 +17,32 @@ def count_avg_price(self):
         }}])
     for x in avg_price:
         return(x)
-
-
-
-
-''''
-def count_houses_exposed(self):
-    houses_seen_today = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    number_houses_exposed = self.df.aggregate([{'$match': {'last_seen_date': houses_seen_today}}, {'$count': 'houses_exposed'}])
-    return number_houses_exposed
-
-def count_houses_sold(self):
     
-'''
+def count_median_price_per_meter(self):
+    median_price_per_meter = self.df['price_per_meter'].median()
+    return median_price_per_meter
+    
+def count_avg_price(self):
+    avg_price = self.collection.aggregate([{
+    '$group': {
+        '_id': 'null',
+        'avg_price': {'$avg': '$price'}
+        }}])
+    for x in avg_price:
+        return(x)
+    
+def count_median_price(self):
+    median_price = self.df['price'].median()
+    return(median_price)
+
+    
+def count_houses_exposed(self):
+    number_houses_exposed = self.collection.aggregate([{'$match': {'last_seen_date': '2023-04-02 20:22:57'}}, {'$count': 'houses_exposed'}])
+    for dokument in number_houses_exposed:
+        return(dokument)
+    
+def count_houses_sold(self):
+    houses_sold = self.collection.aggregate([{'$match': {'last_seen_date': {'$ne': '2023-04-02 20:22:57'}}}, {'$count': 'houses_sold'}])
+    for h_sold in houses_sold:
+        return(h_sold)
+    
