b81556517cc966ddfdad4a7c1c9e5689aafeb6f9
anetapec <aneta.pecka@gmail.com>
2023-04-05 01:04:58 +0200

    data df pandas

--- dashboards/app.py
+++ dashboards/app.py
@@ -2,55 +2,33 @@ import pymongo
 import pandas as pd
 from datetime import datetime
 
-#connection to the database
-def load_collection(self):
-    client = pymongo.MongoClient('mongodb://localhost:27017')
-    db = client['tricity']
-    collection = db['houses']
-    df = pd.DataFrame(list(collection.find()))
-    return df
 
-#calculation of the average price by field price_per_meter
-def count_avg_price_per_meter(self):
-    avg_price_per_meter = self.collection.aggregate([{
-    '$group': {
-        '_id': '$scrapping_date',
-        'avg_price_per_meter': {'$avg': '$price_per_meter'}
-    }}])
-    for x in avg_price_per_meter:
-        return x
-   
-#calculation median price by field price_per_meter    
-def count_median_price_per_meter(self):
-    median_price_per_meter = self.df['price_per_meter'].median()
-    return median_price_per_meter
+class DataSource:
 
-#calculation of the average price by field price    
-def count_avg_price(self):
-    avg_price = self.collection.aggregate([{
-    '$group': {
-        '_id': '$scrapping_date',
-        'avg_price': {'$avg': '$price'}
-    }}])
-    for x in avg_price:
-        return x 
+    def __init__(self):
+        self.df = self.load_collection() 
 
-#calculation median price by field price    
-def count_median_price(self):
-    median_price = self.df['price'].median()
-    return(median_price)
+    #connection to the database
+    def load_collection(self):
+        client = pymongo.MongoClient('mongodb://localhost:27017')
+        db = client['tricity']
+        self.collection = db['houses']
+        df = pd.DataFrame(list(self.collection.find()))
+        return df
 
-#number of houses for sale    
-def count_houses_exposed(self):
-    number_houses_exposed = self.collection.aggregate([{'$match': {'last_seen_date': '2023-04-02 20:22:57'}}, {'$count': 'houses_exposed'}])
-    for dokument in number_houses_exposed:
-        return dokument
-
-#number of houses sold    
-def count_houses_sold(self):
-    houses_sold = self.collection.aggregate([{'$match': {'last_seen_date': {'$ne': '2023-04-02 20:22:57'}}}, {'$count': 'houses_sold'}])
-    for h_sold in houses_sold:
-        return h_sold
-
+    
 if __name__ == '__main__':
-    print("Running File")
+    data_source = DataSource()
+
+    avg_by_price = data_source.df.groupby('scrapping_date')['price'].mean()
+    avg_by_price_per_meter = data_source.df.groupby('scrapping_date')['price_per_meter'].mean()
+    median_by_price = data_source.df.groupby('scrapping_date')['price'].median()
+    median_by_price_per_meter = data_source.df.groupby('scrapping_date')['price_per_meter'].median()
+    number_houses_exposed = data_source.df.value_counts('last_seen_date')['2023-04-04 21:53:02']
+    data_source.df['last_seen_date'] = data_source.df['last_seen_date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+    data_source.df.set_index('last_seen_date', inplace=True)
+    houses_sold_in_a_period_of_time = data_source.df.sort_index().loc['2023-04-02':'2023-04-03']
+    number_houses_sold_per_week = len(houses_sold_in_a_period_of_time)
+    daily_number_of_houses_sold = len(data_source.df.sort_index().loc['2023-04-02'])
+
+    
