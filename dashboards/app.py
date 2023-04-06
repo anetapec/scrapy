2d3cfde893945d34330ca4424c1ad1bc4783a893
anetapec <aneta.pecka@gmail.com>
2023-04-06 10:27:15 +0200

    avg price group date by day

--- dashboards/app.py
+++ dashboards/app.py
@@ -14,18 +14,15 @@ class DataSource:
         self.collection = db['houses']
         df = pd.DataFrame(list(self.collection.find()))
         return df
+# Mean for price per day of houseses sold
+# Median for price per day of houseses sold
 
 if __name__ == '__main__':
     data_source = DataSource()
-    avg_by_price = data_source.df.groupby('scrapping_date')['price'].mean()
-    avg_by_price_per_meter = data_source.df.groupby('scrapping_date')['price_per_meter'].mean()
-    median_by_price = data_source.df.groupby('scrapping_date')['price'].median()
-    median_by_price_per_meter = data_source.df.groupby('scrapping_date')['price_per_meter'].median()
-    number_houses_exposed = data_source.df.value_counts('last_seen_date')['2023-04-04 21:53:02']
-    data_source.df['last_seen_date'] = data_source.df['last_seen_date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-    data_source.df.set_index('last_seen_date', inplace=True)
-    houses_sold_in_a_period_of_time = data_source.df.sort_index().loc['2023-04-02':'2023-04-03']
-    number_houses_sold_per_week = len(houses_sold_in_a_period_of_time)
-    daily_number_of_houses_sold = len(data_source.df.sort_index().loc['2023-04-02'])
-
+    print(data_source.df['scrapping_date'])
+    data_source.df['datetime'] = pd.to_datetime(data_source.df['scrapping_date']) 
+    print(data_source.df['datetime'])
+    groupped_by_day = data_source.df.set_index('datetime').groupby(pd.Grouper(freq='D'))
+    avg_price_by_price = groupped_by_day['price'].mean().astype(int)
+    print(avg_price_by_price)
     
