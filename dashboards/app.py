da54091e438a667315b70c7f2c62b9017d00d0ac
anetapec <aneta.pecka@gmail.com>
2023-04-09 02:04:33 +0200

    dash app

--- dashboards/app.py
+++ dashboards/app.py
@@ -1,6 +1,7 @@
 import pymongo
 import pandas as pd
-from datetime import datetime
+from dash import Dash, html, dcc
+import matplotlib.pyplot as plt
 
 class DataSource:
 
@@ -14,15 +15,53 @@ class DataSource:
         self.collection = db['houses']
         df = pd.DataFrame(list(self.collection.find()))
         return df
-# Mean for price per day of houseses sold
-# Median for price per day of houseses sold
-
-if __name__ == '__main__':
-    data_source = DataSource()
-    print(data_source.df['scrapping_date'])
-    data_source.df['datetime'] = pd.to_datetime(data_source.df['scrapping_date']) 
-    print(data_source.df['datetime'])
-    groupped_by_day = data_source.df.set_index('datetime').groupby(pd.Grouper(freq='D'))
-    avg_price_by_price = groupped_by_day['price'].mean().astype(int)
-    print(avg_price_by_price)
     
+
+data_source = DataSource()
+#converting an object from a date in a string to a Date Time object
+data_source.df['datetime'] = pd.to_datetime(data_source.df['scrapping_date']) 
+print(data_source.df['datetime'])
+#grouping dates by day
+groupped_scraping_date_by_day = data_source.df.set_index('datetime').groupby(pd.Grouper(freq='D'))
+# Mean for price per day of houseses sold
+avg_price_by_price = groupped_scraping_date_by_day['price'].mean().astype(int) #change float=>int
+print(avg_price_by_price)
+
+#grouping dates by day
+groupped_scraping_date_by_day = data_source.df.set_index('datetime').groupby(pd.Grouper(freq='D'))
+# Mean for price per day of houseses sold
+avg_price_by_price = groupped_scraping_date_by_day['price'].mean().astype(int) #change float=>int
+print(avg_price_by_price)
+# Mean for price_per_meter per day of houseses sold
+avg_price_by_price_per_meter = groupped_scraping_date_by_day['price_per_meter'].mean().astype(int)
+print(avg_price_by_price_per_meter)
+# Median for price per day of houseses sold
+median_price_by_price = groupped_scraping_date_by_day['price'].median().astype(int)
+print(median_price_by_price)
+# Median for price_per_meter per day of houseses sold
+median_price_by_price_per_meter = groupped_scraping_date_by_day['price_per_meter'].mean().astype(int)
+print(median_price_by_price_per_meter)
+    
+app = Dash(__name__)
+
+app.layout = html.Div(
+    children = [
+        html.H1(children='Average house price analysis',),
+        html.P(
+            children='Analysis of the average sale price of houses during the day',
+        ),
+    
+    dcc.Graph(
+        figure={
+            'avg_price_by_price': [
+                {
+                    'x': data_source.df['datetime'],
+                    'y': avg_price_by_price,
+                    'type': 'lines'
+                }
+            ],
+        }
+    ),
+])
+if __name__ == "__main__":
+    app.run_server(debug=True)
