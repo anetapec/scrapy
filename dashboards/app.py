2f923e47f0079f33f45af3e16f441b2e28a84581
anetapec <aneta.pecka@gmail.com>
2023-04-17 21:33:05 +0200

    house prices sold per day

--- dashboards/app.py
+++ dashboards/app.py
@@ -19,11 +19,33 @@ class DataSource:
     
     
 data_source = DataSource()
+## house prices sold per day
+# 1) daily mean price 
+daily_avg_price_house_sold = data_source.df.groupby('last_seen_date')['price'].mean()
+print(daily_avg_price_house_sold)
+# 2) daily mean price per meter
+daily_avg_price_per_meter_house_sold = data_source.df.groupby('last_seen_date')['price_per_meter'].mean() 
+print(daily_avg_price_per_meter_house_sold)
 
+# Converting an object 'last_seen_date' from a date in a string to a Date Time object
+data_source.df['last_seen_date'] = pd.to_datetime(data_source.df['last_seen_date'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%Y-%m-%d')
+#print(data_source.df)
+
+# filtr last_seen_date by date
+house_prices_sold_on_a_given_day= data_source.df[(data_source.df['last_seen_date'] == '2023-04-12')]
+# 3) house prices sold on a given day
+house_prices_sold_on_a_given_day_by_price = house_prices_sold_on_a_given_day['price']
+print(house_prices_sold_on_a_given_day_by_price)# Listed house prices by date
+# 4)house prices_per_meters sold on a given day
+house_prices_sold_on_a_given_day_by_price_per_meter = house_prices_sold_on_a_given_day['price_per_meter']
+print(house_prices_sold_on_a_given_day_by_price_per_meter)#
+
+#houses for sale in the time range
 #converting an object 'scrapping_date' from a date in a string to a Date Time object
 data_source.df['datetime'] = pd.to_datetime(data_source.df['scrapping_date']) 
 #print(data_source.df['datetime'])
-#grouping dates by day
+#grouping dates by daytyp = data_source.df['last_seen_date'].value_counts().reset_index()
+
 groupped_scraping_date_by_day = data_source.df.set_index('datetime').groupby(pd.Grouper(freq='D'))
 
 
@@ -71,16 +93,13 @@ daily_number_of_houses_sold_df.to_csv('daily_number_sold.csv')
 df_daily_price_houses_sold = daily_number_of_houses_sold_df[daily_number_of_houses_sold_df.last_seen!='2023-04-16']
 print(df_daily_price_houses_sold)
 df_daily_price_houses_sold.to_csv('df_daily_price_houses_sold.csv')
-#sale prices of houses on particular days
-#price_sold_houses = daily_number_of_houses_sold_df.groupby(data_source.df['last_seen']('price'))
-#print(price_sold_houses)
-
 
 #daily_sale_prices_of_houses_sold = groupped_last_seen_date_by_day['last_seen_date'].count()
 # Number of houses sold per week   
 groupped_last_seen_date_by_week = data_source.df.set_index('last_seen').groupby(pd.Grouper(freq='W'))
 weekly_number_of_houses_sold = groupped_last_seen_date_by_week['last_seen_date'].count()
-print(weekly_number_of_houses_sold)
+weekly_number_of_houses_sold_df = weekly_number_of_houses_sold.to_frame().reset_index()
+print(weekly_number_of_houses_sold_df)
   
 app = Dash(__name__)
 
@@ -111,31 +130,7 @@ app.layout = html.Div(children=[
     )
         
 
-'''''
 
-])
-'''
-        #html.Table([
-        #   html.Tr([html.Th(col) for col in data_source.df.columns])] + 
-        #    [html.Tr([html.Td(data_source.df.iloc[i][col]) for col in data_source.df.columns])for i in range(min_val)]
-        
-
-# dcc.Graph(
-#     figure=go.Figure(
-#         data=[
-#             go.Bar(
-#                 x=data_source.df['datetime'],
-#                 y=data_source.df[avg_price_by_price],
-#                 name='Average price of housing' 
-#             )
-#         ],
-#         layout=go.Layout(
-#                 yaxis_type='log',
-#                 height=300,
-#                 title_text='Mean for price per day of houseses sold'
-#         ) 
-#     )
-# )
         
 
 
@@ -144,3 +139,4 @@ if __name__ == "__main__":
 
 
 
+ 
