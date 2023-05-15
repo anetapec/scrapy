95c23047c4ee8f6d609eed12ba40b685ba4a2dad
anetapec <aneta.pecka@gmail.com>
2023-05-15 23:22:41 +0200

    na tym bazowac

--- dashboards/workship.py
+++ dashboards/workship.py
@@ -16,7 +16,7 @@ class House:
 
     def __init__(self, new_column_name, column_name, column_by_count, frequency, name_collection):
         
-        self.name_collection = self.load_collection(name_collection)
+        self.df = self.load_collection(name_collection)
         self.new_column_name = new_column_name
         self.column_name = column_name
         self.column_by_count = column_by_count
@@ -34,7 +34,7 @@ class House:
     #     return super().load_collection(collection)  # do tego momentu działa!!!!
     
     def groupped_date(self): 
-        self.df = House(self.new_column_name, self.column_name, self.column_by_count, self.frequency, self.name_collection).load_collection(self.name_collection)
+        #self.df = House(self.new_column_name, self.column_name, self.column_by_count, self.frequency, self.name_collection).load_collection(self.name_collection)
         self.df[self.new_column_name] = pd.to_datetime(self.df[self.column_name]) 
         groupped_date_by_freq=self.df.set_index(self.new_column_name).groupby(pd.Grouper(freq = self.frequency)) 
         return groupped_date_by_freq
@@ -43,22 +43,21 @@ class House:
     def avg_price_by_column(self):
         return self.groupped_date()[self.column_by_count].mean().to_frame().reset_index()
 
-    
+    def median_by_column(self):
+        return self.groupped_date()[self.column_by_count].median().to_frame().reset_index()
+
+    def count_houses_for_sale(self):
+        return self.groupped_date()[self.column_name].count().to_frame().reset_index()
 
 
 
 # pobieramy dane z kolekcji houses
-houses = House(new_column_name='datetime', column_name='scrapping_date', column_by_count='price', frequency='W', name_collection='houses')
+houses = House(new_column_name='datetime', column_name='scrapping_date', column_by_count='price', frequency='W', name_collection='flats')
 
 #daily_group = houses.load_collection(collection='houses')
 #print(daily_group)
-collection_houses = houses.load_collection('houses')
+collection_houses = houses.load_collection('flats')
 print(collection_houses)  # ładuje kolekcje
-
-
-#fff = collection_houses()
-#print(fff)
-
 
 
 
