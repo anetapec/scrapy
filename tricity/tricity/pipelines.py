2c440bcc7063a51b736dd899bea6e7f4cd18310f
anetapec <aneta.pecka@gmail.com>
2023-06-11 21:49:35 +0200

    nadal wrzuca do pliku csv wszystko

--- tricity/tricity/pipelines.py
+++ tricity/tricity/pipelines.py
@@ -3,15 +3,16 @@ from tricity import settings
 from datetime import datetime
 import hashlib
 from scrapy.exporters import CsvItemExporter
- 
+import csv
+
 class MongoDBPipeline:
 
-    custom_settings = {
-    'FEEDS': {'scraping_data_csv': {'format': 'csv',}}
-}
-    
-    
-    
+    #custom_settings = {
+    #'FEEDS': {'scraping_data_csv': {'format': 'csv',}}
+#}
+
+
+
     def set_hash(self,item):
         result = hashlib.md5(f"{item['price']}{item['url']}".encode('utf-8'))
         hash_value = result.hexdigest()
@@ -27,37 +28,50 @@ class MongoDBPipeline:
         self.client = pymongo.MongoClient(settings.mongodb_uri)
         db = self.client[settings.mongodb_db]
         self.collection = db[spider_mongo_collection]
-        
-        
-        
-        
+
+
+
+
 
     def close_spider(self, spider):
         self.client.close()
-        self.csv_exporter.finish_exporting()
-        self.csv_file = open('scraping_data_csv', 'wb')
-        self.csv_exporter = CsvItemExporter(self.csv_file)
-        self.csv_exporter.start_exporting()
-        
-        self.csv_file.close()
+
+        # self.csv_file = open('scraping_data_csv', 'wb')
+        # self.csv_exporter = CsvItemExporter(self.csv_file)
+
+        #self.csv_exporter.fields_to_export = ['price', 'url', 'area', 'price_per_meter', 'scrapping_date']
+        # self.csv_exporter.start_exporting()
+
+        # self.csv_exporter.finish_exporting()
+
+        # self.csv_file.close()
+
+
+
+
 
     def process_item(self, item, spider):
         item['price_per_meter'] = (round(float(item['price']) / float(item['area']), 2))
         item['last_seen_date'] = self.scrapping_date
         item['hash'] = self.set_hash(item)
-        filter_dict = {'hash': item['hash']} 
+        filter_dict = {'hash': item['hash']}
         if self.collection.count_documents((filter_dict), limit = 1) !=0:
             new_value = { '$set': {'last_seen_date': item['last_seen_date']}}
-            self.collection.update_one(filter_dict, new_value)    
+            self.collection.update_one(filter_dict, new_value)
         else:
             item['scrapping_date'] = self.scrapping_date
             data = dict(item)
             self.collection.insert_one(data)
-            
+            with open('scraping_data_csv', mode='w') as csv_file:
+                writer = csv.writer(csv_file)
+                scraped_data = self.collection.insert_one(data)
+                writer.writerow(scraped_data)
 
-            
-            
-            self.csv_exporter.export_item(item)
-            
-        
+
+
+
+
+            #self.csv_exporter.export_item(item)
+
+
         return item
--- tricity/tricity/settings.py
+++ tricity/tricity/settings.py
@@ -45,15 +45,15 @@ RANDOMIZE_DOWNLOAD_DELAY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    "tricity.middlewares.TricitySpiderMiddleware": 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    "tricity.middlewares.TricityDownloaderMiddleware": 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
