8f61146874be417d2ee15e6e02bf99f1e2c3a449
anetapec <aneta.pecka@gmail.com>
2023-06-15 23:25:46 +0200

    niestety nie moge zamienić adresu base url

--- tricity/tricity/spiders/flatsspiders.py
+++ tricity/tricity/spiders/flatsspiders.py
@@ -28,24 +28,26 @@ class FlatsspidersSpider(scrapy.Spider):
 
         path_url = response.css(".pagination__controls__next::attr(href)").get()  
         
-        #base_url = "https://dom.trojmiasto.pl/nieruchomosci-rynek-wtorny/e1i,17_14_13_18_16_24_19_25_6_15_61_20_21,ii,1,qi,45_,wi,100.html"
-        #next_page = base_url + path_url
-        # if path_url is not None:
-            # yield scrapy.Request(next_page, callback=self.parse)
+        base_url = "https://dom.trojmiasto.pl/nieruchomosci-rynek-wtorny/e1i,17_14_13_18_16_24_19_25_6_15_61_20_21,ii,1,qi,45_,wi,100.html"
+        next_page = base_url + path_url
+        if path_url is not None:
+            yield scrapy.Request(next_page, callback=self.parse)
         
         # First attempt to solve:
 
-        # base_url = FlatsspidersSpider.start_urls
+        # base_url = self.start_urls
         # next_page = base_url + path_url
-            # #yield scrapy.Request(next_page, callback=self.parse)
+        # if path_url is not None:
+            # yield response.follow(next_page, callback=self.parse)
 
-
-        # Second attempt to solve:
+########################################################################################
+        # Second attempt to solve: - to wg mnie nie działa bo w naszym adresie są 
+            # przecinki i z tego co wyczytałam to może traktowac adres jako krotkę 
         
-        base_url = requests.get(FlatsspidersSpider.start_urls)
-        next_page = base_url.url + path_url
-        if path_url is not None:
-            yield response.follow(next_page, callback=self.parse)
+        # base_url = requests.post(self.start_urls)    # tu próbowałam też z request.get
+        # next_page = base_url.url + path_url
+        # if path_url is not None:
+            # yield scrapy.Request(next_page, callback=self.parse)
 
 
         
