3fb47ba7dd091387003c94d62366e658a78f0cba
anetapec <aneta.pecka@gmail.com>
2023-06-24 00:19:01 +0200

    automation minkube

--- automation/minikub_start.py
+++ automation/minikub_start.py
@@ -1,48 +1,12 @@
 
 import subprocess
-import fs 
-import os
 
-'''
-export CHANGE_MINIKUBE_NONE_USER=true
-sudo sysctl fs.protected_regular=0
-sudo -E minikube start --driver=none --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf
-'''
+result1 = subprocess.Popen([f"export CHANGE_MINIKUBE_NONE_USER=true" ], shell=True)
+print(result1.communicate())
 
+result2 = subprocess.Popen([f"sudo sysctl fs.protected_regular=0" ], shell=True)
+print(result2.communicate())
 
-name = 'CHANGE_MINIKUBE_NONE_USER'
-p = subprocess.Popen(f"export {name}", shell=True)
-print(p.communicate())
+result2 = subprocess.Popen([f"sudo -E minikube start --driver=none --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf"], shell=True)
+print(result2.communicate())
 
-p = subprocess.Popen(f'["sudo", "sysctl", "fs.protected_regular=0"]')
-print(p.communicate())
-
-path = "/run/systemd/resolve/resolv.conf"
-os
-# p = subprocess.Popen('sudo -E minikube start --driver=none --ext.ra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf')
-
-#print(p.communicate())
-
-# name_n = 'protected_regular=0'
-# n = subprocess.Popen(f"sudo sysctl fs.{name_n}")
-# print(n.communicate())
-
-l = subprocess.Popen(f'["sudo", "-E minikube start",  "--driver=none", "--extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf"]')
-print(l.communicate())
-
-
-
-
-
-'''
-name = 'CHANGE_MINIKUBE_NONE_USER'
-p = subprocess.Popen(f"export {name}", shell=True)
-print(p.communicate())
-
-n = subprocess.Popen(f"sudo sysctl fs.protected_regular=0")
-print(n.communicate())
-
-l = subprocess.Popen(f"sudo -E minikube start --driver=none ")
-print(l.communicate())
-
-'''
